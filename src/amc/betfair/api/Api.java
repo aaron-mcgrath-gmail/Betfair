82ca3d1d91e134ca67ef508cb5deb7096619c73f
aaron-mcgrath-adp <aaron.mcgrath@adaptris.com>
2015-06-16 22:20:08 +0100

    Added more methods into the API.  We can now get prices for a particular market.

--- src/amc/betfair/api/Api.java
+++ src/amc/betfair/api/Api.java
@@ -1,6 +1,9 @@
 package amc.betfair.api;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -8,11 +11,16 @@ import java.util.Set;
 import com.betfair.aping.api.ApiNgJsonRpcOperations;
 import com.betfair.aping.api.ApiNgOperations;
 import com.betfair.aping.entities.EventTypeResult;
+import com.betfair.aping.entities.MarketBook;
 import com.betfair.aping.entities.MarketCatalogue;
 import com.betfair.aping.entities.MarketFilter;
+import com.betfair.aping.entities.PriceProjection;
 import com.betfair.aping.entities.TimeRange;
 import com.betfair.aping.enums.MarketProjection;
 import com.betfair.aping.enums.MarketSort;
+import com.betfair.aping.enums.MatchProjection;
+import com.betfair.aping.enums.OrderProjection;
+import com.betfair.aping.enums.PriceData;
 import com.betfair.aping.exceptions.APINGException;
 
 public class Api {
@@ -45,9 +53,19 @@ public class Api {
     }
   }
 
-  public MarketCatalogue getNextMarket(String eventTypeID) throws ApiException {
+  public List<MarketCatalogue> getMarketsToday(String eventTypeID) throws ApiException {
+    GregorianCalendar startToday = new GregorianCalendar();
+    startToday.set(Calendar.HOUR_OF_DAY, 0);
+    GregorianCalendar endToday = new GregorianCalendar();
+    endToday.set(Calendar.HOUR_OF_DAY, 23);
+    
+    return this.getMarketsWithinDateRange(eventTypeID, startToday.getTime(), endToday.getTime());
+  }
+  
+  public List<MarketCatalogue> getMarketsWithinDateRange(String eventTypeID, Date startDate, Date endDate) throws ApiException {
     TimeRange time = new TimeRange();
-    time.setFrom(new Date());
+    time.setFrom(startDate);
+    time.setTo(endDate);
 
     Set<String> countries = new HashSet<String>();
     countries.add("GB");
@@ -67,23 +85,30 @@ public class Api {
     Set<MarketProjection> marketProjection = new HashSet<MarketProjection>();
     marketProjection.add(MarketProjection.RUNNER_DESCRIPTION);
 
-    String maxResults = "1";
-
     try {
-      List<MarketCatalogue> marketCatalogueResult = jsonOperations
-          .listMarketCatalogue(marketFilter, marketProjection, MarketSort.FIRST_TO_START, maxResults, applicationKey, sessionToken);
-      
-      if(marketCatalogueResult != null) {
-
-        if(marketCatalogueResult.size() == 1)
-          return marketCatalogueResult.get(0);
-        else
-          return null;
-      } else
-        return null;
-      
+      return jsonOperations.listMarketCatalogue(marketFilter, marketProjection, MarketSort.FIRST_TO_START, "0", this.getApplicationKey(), this.getSessionToken());
     } catch (APINGException e) {
       throw new ApiException(e);
+    }
+  }
+  
+  public List<MarketBook> getMarketData(String marketId) throws ApiException {
+    PriceProjection priceProjection = new PriceProjection();
+    Set<PriceData> priceData = new HashSet<PriceData>();
+    priceData.add(PriceData.EX_BEST_OFFERS);
+
+    //In this case we don't need these objects so they are declared null
+    OrderProjection orderProjection = null;
+    MatchProjection matchProjection = null;
+    String currencyCode = null;
+
+    List<String> marketIds = new ArrayList<String>();
+    marketIds.add(marketId);
+
+    try {
+      return jsonOperations.listMarketBook(marketIds, priceProjection, orderProjection, matchProjection, currencyCode, applicationKey, sessionToken);
+    } catch (APINGException ex) {
+      throw new ApiException(ex);
     }
   }
   
